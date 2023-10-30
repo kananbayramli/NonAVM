e111afb36d6de95a14c7be296cc395de2e2f3cf8
kananbayramli <bayramli-kenan@mail.ru>
2023-10-30 23:23:04 +0400

    Role CRUD done

--- presentation/WebUI/Areas/Admin/Controllers/RoleController.cs
+++ presentation/WebUI/Areas/Admin/Controllers/RoleController.cs
@@ -1,7 +1,9 @@
 ﻿using ECommerse.Core.Identity;
 using ECommerse.WebUI.Controllers;
+using ECommerse.WebUI.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ECommerse.WebUI.Areas.Admin.Controllers
 {
@@ -14,12 +16,47 @@ namespace ECommerse.WebUI.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var roles = roleManager.Roles;
+            return View(roles.Select(r => new RoleViewModel { Id = r.Id, Name = r.Name }));
         }
 
         public IActionResult AddRole()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel roleViewModel)
+        {
+            var result = await roleManager.CreateAsync(new AppRole() { Name = roleViewModel.Name });
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(Guid id)
+        {
+            var role = await roleManager.FindByIdAsync(id.ToString());
+            if (role is not null)
+                return View(new RoleViewModel { Id = role.Id, Name = role .Name});
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(RoleViewModel roleViewModel)
+        {
+            await roleManager.UpdateAsync(new AppRole { Id = roleViewModel.Id, Name = roleViewModel.Name });
+            return RedirectToAction("Index", "Role");
+        }
+
+        public async Task<IActionResult> DeleteRole(Guid id)
+        {
+            var role = await roleManager.FindByIdAsync(id.ToString());
+            if (role is not null)
+            {
+                await roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ presentation/WebUI/ViewModels/RoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerse.WebUI.ViewModels
+{
+    public class RoleViewModel
+    {
+        [Display(Name = "Role ismi")]
+        [Required(ErrorMessage = "Role ismi gereklidir")]
+        public string Name { get; set; } = null!;
+
+        public string Id { get; set; } = null!;
+    }
+}
