4e57482d8ba0f7b8052d6ff23946bd9dfa4197aa
Mutalib Gozalov <mutalibshg@code.edu.az>
2023-11-25 05:12:03 -0800

    Logo prop added to Brand entity(migration genereated)

--- src/Core/Entities/Brand.cs
+++ src/Core/Entities/Brand.cs
@@ -6,6 +6,7 @@ public class Brand : BaseEntity
 {
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
+    public string? Logo { get; set; }
 
     public ICollection<Product> Products { get; set; } = null!;
     public ICollection<Category> Categories { get; set; } = null!;
